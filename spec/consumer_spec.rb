b94b1e643cb3ac6a597dafea3dac4605af9256b6
lars <larsburgess@gmail.com>
2011-09-22 11:35:09 -0700

    added save method

--- spec/consumer_spec.rb
+++ spec/consumer_spec.rb
@@ -14,7 +14,7 @@ describe RhoGallery::Consumer do
     end
     
     it "should make a GET request to get a consumer by ID" do
-      RhoGallery::Consumer.find_by_id("4e6146d5bdd0c8048c000004")
+      RhoGallery::Consumer.find("4e6146d5bdd0c8048c000004")
       WebMock.should have_requested(:get, RhoGallery.resource_url("testuser", "consumers/4e6146d5bdd0c8048c000004"))
     end
   end
@@ -22,17 +22,17 @@ describe RhoGallery::Consumer do
   context "Create, update and Delete actions" do
     it "should send a POST request to save a new consumer" do
       new_cons = RhoGallery::Consumer.new({:login => "testconsumer", :name => "this is a name"})
-      new_cons.create_new
+      new_cons.save
       WebMock.should have_requested(:post, RhoGallery.resource_url("testuser", "consumers"))
     end
     it "should send a PUT request to update a consumer" do
-      consumer = RhoGallery::Consumer.find_by_id("4e6146d5bdd0c8048c000004")
+      consumer = RhoGallery::Consumer.find("4e6146d5bdd0c8048c000004")
       consumer.login = "new_login_for_this_consumer"
       consumer.update
       WebMock.should have_requested(:put, RhoGallery.resource_url("testuser", "consumers/4e6146d5bdd0c8048c000004"))
     end
     it "should send a DELETE request to delete a consumer" do
-      consumer = RhoGallery::Consumer.find_by_id("4e6146d5bdd0c8048c000004")
+      consumer = RhoGallery::Consumer.find("4e6146d5bdd0c8048c000004")
       consumer.delete
       WebMock.should have_requested(:delete, RhoGallery.resource_url("testuser", "consumers/4e6146d5bdd0c8048c000004"))
     end
@@ -40,14 +40,14 @@ describe RhoGallery::Consumer do
       stub_request(:post, RhoGallery.resource_url("testuser", "consumers")).
         to_return({:status => 422, :body => "login can't be blank"})
       new_cons = RhoGallery::Consumer.new({:name => "this is a name"})
-      new_cons.create_new
+      new_cons.save
       WebMock.should have_requested(:post, RhoGallery.resource_url("testuser", "consumers"))
       new_cons.errors.should == "login can't be blank"
     end
     it "should respond with error when is trying to update a consumer with wrong attributes" do
       stub_request(:put, RhoGallery.resource_url("testuser", "consumers/4e6146d5bdd0c8048c000004")).
         to_return({:status => 422, :body => "login can't be blank"})
-      consumer = RhoGallery::Consumer.find_by_id("4e6146d5bdd0c8048c000004")
+      consumer = RhoGallery::Consumer.find("4e6146d5bdd0c8048c000004")
       consumer.login = ""
       consumer.update
       WebMock.should have_requested(:put, RhoGallery.resource_url("testuser", "consumers/4e6146d5bdd0c8048c000004"))
@@ -57,7 +57,7 @@ describe RhoGallery::Consumer do
   
   context "Get and set consumers values" do
     before(:each) do
-      @consumer = RhoGallery::Consumer.find_by_id("4e6146d5bdd0c8048c000004")
+      @consumer = RhoGallery::Consumer.find("4e6146d5bdd0c8048c000004")
     end
     
     it "should get the consumer values and set new values" do
--- spec/group_spec.rb
+++ spec/group_spec.rb
@@ -14,7 +14,7 @@ describe RhoGallery::Group do
     end
     
     it "should make a GET request when is finding a group by ID" do
-      RhoGallery::Group.find_by_id("4e6146d5bdd0c8048c000004")
+      RhoGallery::Group.find("4e6146d5bdd0c8048c000004")
       WebMock.should have_requested(:get, RhoGallery.resource_url("testuser", "groups/4e6146d5bdd0c8048c000004"))
     end
   end
@@ -22,11 +22,11 @@ describe RhoGallery::Group do
   context "Create and delete actions" do
     it "should send a POST request when is creating a new group" do
       new_group = RhoGallery::Group.new({:name => "group_name"})
-      new_group.create_new()
+      new_group.save
       WebMock.should have_requested(:post, RhoGallery.resource_url("testuser", "groups"))
     end
     it "should send a DELETE request when is deleting a group" do
-      group = RhoGallery::Group.find_by_id("4e6146d5bdd0c8048c000004")
+      group = RhoGallery::Group.find("4e6146d5bdd0c8048c000004")
       group.delete
       WebMock.should have_requested(:delete, RhoGallery.resource_url("testuser", "groups/4e6146d5bdd0c8048c000004"))
     end
@@ -34,7 +34,7 @@ describe RhoGallery::Group do
       stub_request(:post, RhoGallery.resource_url("testuser", "groups")).
         to_return({:status => 422, :body => "name can't be blank"})
       new_group = RhoGallery::Group.new
-      new_group.create_new
+      new_group.save
       WebMock.should have_requested(:post, RhoGallery.resource_url("testuser", "groups"))
       new_group.errors.should == "name can't be blank"
     end
@@ -42,7 +42,7 @@ describe RhoGallery::Group do
   
   context "Get groups values" do
     it "should get al the group values" do
-      group = RhoGallery::Group.find_by_id("4e6146d5bdd0c8048c000004")
+      group = RhoGallery::Group.find("4e6146d5bdd0c8048c000004")
       group.name.should == "koombea"
       group.id.should == "4e6146d5bdd0c8048c000004"
     end
@@ -50,7 +50,7 @@ describe RhoGallery::Group do
   
   context "Update method doesn't exist for groups" do
     it "should raise a NotMethod Exception when is trying to update a group" do
-      group = RhoGallery::Group.find_by_id("4e6146d5bdd0c8048c000004")
+      group = RhoGallery::Group.find("4e6146d5bdd0c8048c000004")
       lambda { group.update }.should raise_error
     end
   end
